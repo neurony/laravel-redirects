26a794e7e3a49cb60e8c26435eeec1c3a84d031e
Andrei Badea <zbiller@gmail.com>
2019-06-02 15:21:37 +0300

    created redirect model interface

---
+++ src/Contracts/RedirectModelContract.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Neurony\Redirects\Contracts;
+
+interface RedirectModelContract
+{
+    /**
+     * @param string $value
+     */
+    public function setOldUrlAttribute($value);
+
+    /**
+     * @param string $value
+     */
+    public function setNewUrlAttribute($value);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $url
+     * @return mixed
+     */
+    public function scopeWhereOldUrl($query, string $url);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $url
+     * @return mixed
+     */
+    public function scopeWhereNewUrl($query, string $url);
+
+    /**
+     * @return array
+     */
+    public static function getStatuses() : array;
+
+    /**
+     * @param RedirectModelContract $model
+     * @param string $finalUrl
+     * @return void
+     */
+    public function syncOldRedirects(RedirectModelContract $model, string $finalUrl): void;
+
+    /**
+     * @param string $path
+     * @return RedirectModelContract|null
+     */
+    public static function findValidOrNull($path): RedirectModelContract;
+}
